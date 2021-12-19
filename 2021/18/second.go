f58944b2cf9d86b11c7bf6bdca10a8330548acbb
pglock <phil.glock@gmail.com>
2021-12-19 01:04:09 +0100

    day 18

---
+++ 2021/18/second.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type snailNumber struct {
+	val int
+	depth int
+}
+
+func readData(filename string) ([][]snailNumber, []string) {
+	file, _ := os.Open(filename)
+	scanner := bufio.NewScanner(file)
+	numbers := make([][]snailNumber, 0)
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		lineStr := scanner.Text()
+		lines = append(lines, lineStr)
+		number := make([]snailNumber, 0)
+		depth := 0
+		for _, char := range lineStr {
+			switch char {
+			case '[':
+				depth++
+				continue
+			case ']':
+				depth--
+				continue
+			case ',':
+				continue
+			default:
+				num, _ := strconv.Atoi(string(char))
+				number = append(number, snailNumber{num, depth})
+			}
+		}
+		numbers = append(numbers, number)
+	}
+	return numbers, lines
+}
+
+func reduce(number []snailNumber) []snailNumber {
+	changed := true
+	for changed {
+		changed = false
+		for index:= range number {
+			if number[index].depth >= 5 {
+				if index > 0 {
+					number[index-1].val += number[index].val
+				}
+				if index + 2 < len(number) {
+					number[index +2].val += number[index+1].val
+				}
+				number[index].val = 0
+				number[index].depth--
+				number = append(number[:index+1], number[index+2:]...)
+				changed = true
+				break
+			}
+		}
+
+		if changed {
+			continue
+		}
+
+		for index := range number {
+			if number[index].val > 9 {
+				left := number[index].val / 2
+				right := left
+				if number[index].val % 2 != 0 {
+					right += 1
+				}
+				newDepth := number[index].depth + 1
+				sleft := snailNumber{left, newDepth}
+				sright := snailNumber{right, newDepth}
+				number = append(number[:index], append([]snailNumber{sleft, sright}, number[index+1:]...)...)
+				changed = true
+				break
+			}
+		}
+	}
+
+	return number
+}
+
+func add(left []snailNumber, right []snailNumber) []snailNumber {
+	newNumber := append(left, right...)
+
+	for i := range newNumber {
+		newNumber[i].depth++
+	}
+	return newNumber
+}
+
+func magnitude(number []snailNumber) int {
+	fmt.Println(number)
+	for depth:=4; depth>0; depth-=1 {
+		newNumber := make([]snailNumber, 0)
+		for i:=0;i<len(number);i++ {
+			if number[i].depth == depth {
+				val := 3 * number[i].val + 2 * number[i+1].val
+				newNumber = append(newNumber, snailNumber{val, depth-1})
+				i += 1
+			} else {
+				newNumber = append(newNumber, number[i])
+			}
+		}
+		number = newNumber
+	}
+	return number[0].val
+}
+
+func main() {
+	numbers, _ := readData("input.txt")
+	maxVal := -1
+	for i:=0;i<len(numbers);i++ {
+		for j:=0;j<len(numbers); j++{
+			if i == j {
+				continue
+			}
+			left := make([]snailNumber, len(numbers[i]))
+			right := make([]snailNumber, len(numbers[j]))
+			copy(left, numbers[i])
+			copy(right, numbers[j])
+			number := add(left, right)
+			number = reduce(number)
+			val := magnitude(number)
+			if val > maxVal {
+				maxVal = val
+			}
+		}
+	}
+	fmt.Println(maxVal)
+}
